71f07f59cb96807eb7f781619f6578f3ad668b0a
karinehoffman <khoffman2016@kellogg.northwestern.edu>
2015-04-25 23:19:52 -0500

    def_loan_payment completed

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -34,7 +34,7 @@ class CalculationsController < ApplicationController
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    @monthly_payment = ((@apr/12)*@principal)/(1-((1+(@apr/12))**(@years*-12)))
   end
 
   def time_between
