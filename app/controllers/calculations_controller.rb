8808f3af8ec7a2ddbaadab47a814648a83c60a31
chhavo <cadtani@gmail.com>
2015-04-26 10:21:04 -0500

    word_count_complete

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -11,13 +11,14 @@ class CalculationsController < ApplicationController
     # ================================================================================
 
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    @character_count_with_spaces = @text.length
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    @character_count_without_spaces = @text.length - @text.count(' ')
 
-    @word_count = "Replace this string with your answer."
+    @word_count = @text.split(' ').count
 
-    @occurrences = "Replace this string with your answer."
+    @occurrences = @text.scan(/#{@special_word}/).count
+
   end
 
   def loan_payment
