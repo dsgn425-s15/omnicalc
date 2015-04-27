50a5998e2d7ed61ae431bcd11f144f8e070382a8
dmetzel <dmetzel2016@kellogg.northwestern.edu>
2015-04-27 11:57:02 -0500

    Completed HW

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -4,20 +4,12 @@ class CalculationsController < ApplicationController
     @text = params[:user_text]
     @special_word = params[:user_word]
 
-    # ================================================================================
-    # Your code goes below.
-    # The text the user input is in the string @text.
-    # The special word the user input is in the string @special_word.
-    # ================================================================================
+@character_count_with_spaces = @text.length
+@character_count_without_spaces = @text.delete(' ').length
 
+    @word_count = @text.split.size
 
-    @character_count_with_spaces = "Replace this string with your answer."
-
-    @character_count_without_spaces = "Replace this string with your answer."
-
-    @word_count = "Replace this string with your answer."
-
-    @occurrences = "Replace this string with your answer."
+    @occurrences = @text.scan(@special_word).length
   end
 
   def loan_payment
@@ -25,65 +17,54 @@ class CalculationsController < ApplicationController
     @years = params[:number_of_years].to_i
     @principal = params[:principal_value].to_f
 
-    # ================================================================================
-    # Your code goes below.
-    # The annual percentage rate the user input is in the decimal @apr.
-    # The number of years the user input is in the integer @years.
-    # The principal value the user input is in the decimal @principal.
-    # ================================================================================
-
-    @monthly_payment = "Replace this string with your answer."
+    @monthly_payment = @apr/100/12*@principal/(1-(1+@apr/100/12)**(-@years*12))
   end
 
   def time_between
     @starting = Chronic.parse(params[:starting_time])
     @ending = Chronic.parse(params[:ending_time])
 
-    # ================================================================================
-    # Your code goes below.
-    # The start time is in the Time @starting.
-    # The end time is in the Time @ending.
-    # Note: Ruby stores Times in terms of seconds since Jan 1, 1970.
-    #   So if you subtract one time from another, you will get an integer
-    #   number of seconds as a result.
-    # ================================================================================
-
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = @ending-@starting
+    @minutes = ((@seconds)/60).round(2)
+    @hours = (@minutes/60).round(2)
+    @days = (@hours/24).round(2)
+    @weeks = (@days/7).round(2)
+    @years = (@days/365).round(2)
   end
 
   def descriptive_statistics
     @numbers = params[:list_of_numbers].gsub(',', '').split.map(&:to_f)
 
-    # ================================================================================
-    # Your code goes below.
-    # The numbers the user input are in the array @numbers.
-    # ================================================================================
+sorted = @numbers.sort
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = sorted
 
-    @count = "Replace this string with your answer."
+    @count = @numbers.length
 
-    @minimum = "Replace this string with your answer."
+    @minimum = sorted.at(0)
 
-    @maximum = "Replace this string with your answer."
+    @maximum = sorted.at(-1)
 
-    @range = "Replace this string with your answer."
+    @range = sorted.at(-1)-sorted.at(0)
 
-    @median = "Replace this string with your answer."
 
-    @sum = "Replace this string with your answer."
 
-    @mean = "Replace this string with your answer."
+    @median = (sorted[(sorted.length - 1) / 2] + sorted[sorted.length / 2]) / 2.0
 
-    @variance = "Replace this string with your answer."
+    @sum = @numbers.inject(:+)
 
-    @standard_deviation = "Replace this string with your answer."
+    @mean = @numbers.inject(:+)/@numbers.length
 
-    @mode = "Replace this string with your answer."
+@variance = 0
+for i in 0..(@numbers.length-1)
+@variance += (@numbers[i] - @mean)**2
+end
+
+
+    @standard_deviation = Math.sqrt(@variance).round(2)
+
+freq = @numbers.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+
+    @mode = @numbers.max_by {|v| freq[v]}
   end
 end
