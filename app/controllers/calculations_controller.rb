e71a63214b47d69aaff5b5626e5b0d98fb4e2cce
nicoleroden <nicole.roden@gmail.com>
2015-04-27 10:52:04 -0500

    mode fix

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -95,7 +95,7 @@ end
         median = ((@sorted_numbers [left] + @sorted_numbers[right])/2).to_f
     end
 
-    @median = median
+    @median = median.to_f
 
     def sum(list_of_numbers)
         running_total = 0
@@ -139,20 +139,55 @@ end
 
 def mode(list_of_numbers)
 
-        list_of_numbers.each do |check|
-
-        @number = 1
-        count_@number = list_of_numbers.count("#{check}")
-        line_@number = {:number => "#{check}", :count =>"#{count_@number}"}
         count_table = []
-        count_table.push (line_@number)
-        @number = @number+1
+
+        list_of_numbers.each do |check|
+        count = list_of_numbers.count(check)
+        line={:number => check, :count =>count}
+    count_table.push (line)
+
     end
-        count_table.sort
-        return count_table [0] [:number]
+        count_table.sort_by{|element|element[:count]}
+        return count_table[@count-1][:number]
 end
 
     @mode = mode(@sorted_numbers)
 
   end
 end
+
+
+#mode notes:
+#I see where you are going with this, but you can't use the @ sign in the middle of a variable name; either at the beginning, to turn it in to an instance variable, or not at all.
+
+#It seems like you are trying to create new variables count_1, count_2, etc with this technique, which is a great idea. But this syntax is not legal.
+
+#Instead, move your count_table = [] line above your .each. Right now, since it is inside, you are resetting it to a blank array every time through the loop. If you create it one time outside the loop, it will grow as you push new elements in to it.
+
+#Then you won't need to deal with incrementing @number yourself. You can get rid of all that and simply do
+
+#count_table.push(line)
+#By the way, be careful with your spaces. Never put a space between a method name and its parentheses for arguments. Never put a space between an array/hash and its square brackets for accessing. Also, be careful not to overuse String Interpolation:
+
+#{}"#{check}"
+#{}"#{count}"
+#etc
+#It's just a handy shortcut when you need to add several strings together. In this case, your code is short for:
+
+#{}"" + count.to_s + ""
+#But to what purpose? I think you just want to use count and check as is, as numbers.
+
+#Finally, once you have an array of the form
+
+# count_table
+#[
+#  { :number => 1.0, :count => 3},
+#  { :number => 9.0, :count => 1},
+#  { :number => 3.0, :count => 9},
+#  { :number => 8.0, :count => 1}
+#]
+#Plain old .sort won't work by itself, because it won't know whether to sort by comparing the :number or the :count. You instead have to do
+
+#count_table.sort_by { |element| element[:count] }
+#And remember that it will sort in ascending order, not descending.
+
